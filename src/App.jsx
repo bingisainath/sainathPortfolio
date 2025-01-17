9ed0b7a8b0f0d3171580313e0433f581559547e9
bingisainath <bingisainath207@gmail.com>
2025-01-17 12:36:16 +0530

    welcome screen

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Home from "./Pages/Home";
 import About from "./Pages/About";
 import AnimatedBackground from "./components/Background";
 import Navbar from "./components/Navbar";
-import Portofolio from "./Pages/Portofolio";
-import ContactPage from "./Pages/Contact";
+// import Portofolio from "./Pages/Portofolio";
+// import ContactPage from "./Pages/Contact";
 import ProjectDetails from "./components/ProjectDetail";
 import WelcomeScreen from "./Pages/WelcomeScreen";
 import { AnimatePresence } from 'framer-motion';
@@ -14,11 +14,12 @@ import { AnimatePresence } from 'framer-motion';
 const LandingPage = ({ showWelcome, setShowWelcome }) => {
   return (
     <>
-      <AnimatePresence mode="wait">
+      {/* <AnimatePresence mode="wait">
         {showWelcome && (
           <WelcomeScreen onLoadingComplete={() => setShowWelcome(false)} />
         )}
-      </AnimatePresence>
+        <WelcomeScreen />
+      </AnimatePresence> */}
 
       {!showWelcome && (
         <>
@@ -26,9 +27,9 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
           <AnimatedBackground />
           <Home />
           <About />
-          <Portofolio />
-          <ContactPage />
-          <footer>
+          {/* <Portofolio /> */}
+          {/* <ContactPage /> */}
+          {/* <footer>
             <center>
               <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
               <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
@@ -39,7 +40,7 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
                 . All Rights Reserved.
               </span>
             </center>
-          </footer>
+          </footer> */}
         </>
       )}
     </>
